52c9fb0c8764f7b6016a3cb7916ef62552caf021
LaNaya <lanaya@sjtu.edu.cn>
2023-01-03 20:40:58 +0800

    add double_dqn algorithm

--- algorithms/model_free/online/value_based/dqn_variants/double_dqn.py
+++ algorithms/model_free/online/value_based/dqn_variants/double_dqn.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 import sys
 sys.path.append(r"C:\Users\lanaya\Desktop\DRLAlgorithms")
-from typing import Any, Optional, Union, Dict
+from typing import Any, Dict, Optional, Union, Tuple
 import gym
 import gym.spaces as spaces
 import random
+import numpy as np
 import torch
 import torch.nn.functional as F
-import numpy as np
-from common.envs import Monitor, VecEnv
 from dqn import DQN
+from common.envs import Monitor, VecEnv
+from common.models import DeepQNetwork
 from common.buffers import ReplayBuffer
-from common.models import BootstrappedQNetwork
 from common.utils.utils import Mish, obs_to_tensor
-
-class BootstrappedDQN(DQN):
+    
+class DDQN(DQN):
     def __init__(self, 
                  env: Union[Monitor, VecEnv], 
                  rollout_steps: int = 16,
                  total_timesteps: int = 1e6, 
                  gradient_steps: int = 4,
                  qnet_kwargs: Optional[Dict[str, Any]] = None,
-                 num_heads: int = 10,
                  learning_start: int = 1000,
                  buffer_size: int = 10000,
                  batch_size: int = 256,
@@ -37,9 +36,7 @@ class BootstrappedDQN(DQN):
                  seed: Optional[int] = None,
                 ):
         
-        self.num_heads = num_heads
-        
-        super(BootstrappedDQN, self).__init__(
+        super(DDQN, self).__init__(
                  env, 
                  rollout_steps,
                  total_timesteps, 
@@ -58,54 +55,9 @@ class BootstrappedDQN(DQN):
                  log_interval,
                  device,
                  seed,
-            )
-        
-    def _setup_model(self):
-        if isinstance(self.env.observation_space, spaces.Box):
-            self.observation_dim = self.env.observation_space.shape[0]
-        elif isinstance(self.env.observation_space, spaces.Discrete):
-            self.observation_dim = 1
-        
-        self.num_actions = self.env.action_space.n
-        
-        self.qnet = BootstrappedQNetwork(self.observation_dim, self.num_actions, self.num_heads, **self.qnet_kwargs).to(self.device)
-        self.target_qnet = BootstrappedQNetwork(self.observation_dim, self.num_actions, self.num_heads, **self.qnet_kwargs).to(self.device)
-        self.target_qnet.load_state_dict(self.qnet.state_dict())
-            
-        if self.verbose > 0:
-            print(self.qnet) 
+                )
 
-        self.buffer = ReplayBuffer(self.buffer_size, self.device)
-        
-        self.obs = self.env.reset()
-
-    def _rollout(self) -> None:
-        self.head_id = random.randint(0, self.num_heads-1)
-        
-        for i in range(self.rollout_steps):
-            q = self.qnet(obs_to_tensor(self.obs), self.head_id).to(self.device)
-            
-            coin = random.random()
-            if coin < self.current_eps:
-                action = [random.randint(0, self.env.action_space.n - 1) for _ in range(self.env.num_envs)]
-                action = np.asarray(action)[:, np.newaxis]
-            else:
-                action = q.argmax(dim=-1, keepdim=True).cpu().detach().numpy()
-
-            next_obs, reward, done, info = self.env.step(action)
-
-            self.buffer.add((self.obs, action, reward, next_obs, done))
-
-            self.obs = next_obs
-            
-            self.current_timesteps += self.env.num_envs
-            
-            self._update_episode_info(info)
-            
-            self._update_exploration_eps()
-            
-                
-    def _train(self) -> None:
+    def _train(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         obs, actions, rewards, next_obs, dones = self.buffer.sample(self.batch_size)
             
         actions = actions.type("torch.LongTensor").to(self.device)
@@ -116,12 +68,14 @@ class BootstrappedDQN(DQN):
         assert isinstance(next_obs, torch.Tensor) and next_obs.shape[1] == self.observation_dim
         assert isinstance(dones, torch.Tensor) and dones.shape[1] == 1
             
-        q_next = self.target_qnet(next_obs, self.head_id)
-        q_next = q_next.max(dim=1, keepdim=True)[0]
-            
+        q_next = self.qnet(next_obs)        
+        next_acts = q_next.max(dim=1, keepdim=True)[1]
+        q_next = self.target_qnet(next_obs)
+        q_next = q_next.gather(dim=1, index=next_acts)
+        
         q_target = rewards + self.gamma * (1 - dones) * q_next
             
-        q_values = self.qnet(obs, self.head_id)
+        q_values = self.qnet(obs)
             
         q_a = q_values.gather(1, actions)
 
@@ -133,20 +87,22 @@ class BootstrappedDQN(DQN):
 
         if self.training_iterations % self.target_update_interval == 0:
             self.target_qnet.load_state_dict(self.qnet.state_dict())
-    
+                
+        return (obs, actions, rewards, next_obs, dones)
+
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
     env = Monitor(env)
-    bootdqn = BootstrappedDQN(env, 
+    #env = VecEnv(env, num_envs=4)
+    ddqn = DDQN(env, 
+              rollout_steps=8,
               total_timesteps=1e6,
               gradient_steps=1,
-              rollout_steps=8,
-              qnet_kwargs={"activation_fn": Mish, "optimizer_kwargs":{"lr":1e-3}},
-              num_heads=2,
+              qnet_kwargs={"activation_fn": Mish, "optimizer_kwargs":{"lr":1e-3}}, 
               learning_start=500,
               buffer_size=5000,
               batch_size=64,
               log_dir=None,
               log_interval=20,
-              seed=10,)
-    bootdqn.learn()
+              seed=2,)
+    ddqn.learn()
